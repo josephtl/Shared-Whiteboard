d34a428a7334e012f4343e542fb5ee3247f15584
josephtl <72643627+josephtl@users.noreply.github.com>
2023-08-19 17:42:40 +1000

    Update KickUser.java

--- KickUser.java
+++ KickUser.java
@@ -19,19 +19,6 @@ public class KickUser extends JDialog {
 	private final JPanel contentPanel = new JPanel();
 
 	/**
-	 * Launch the application.
-	 */
-//	public static void main(String[] args) {
-//		try {
-//			KickUser dialog = new KickUser();
-//			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-//			dialog.setVisible(true);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-
-	/**
 	 * Create the dialog.
 	 */
 	public KickUser(String username, int index, IRemoteWhiteBoard remoteWB, DefaultListModel<String> listModel) {
